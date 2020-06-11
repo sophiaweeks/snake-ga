6acdbb74c379f9f2983fb5d11a21c735597f3d58
mark <gmark@pdx.edu>
2020-06-11 02:12:59 -0700

    Modified NEAT settings and fitness function (good perfomance).

--- NEAT.py
+++ NEAT.py
@@ -3,38 +3,47 @@ from Game import Game
 import neat
 import os
 import numpy as np
-import random
-
-EPSILON = 0.1
 
 
 def play(network, num_games, display_on):
     total_score = 0
 
     for i in range(num_games):
-        game = Game(400, 400, 0)
+        game = Game(440, 440, 0)
+        move_count = 0
+        current_score = 0
 
         if display_on:
             game.update_display()
 
         while not game.crash:
 
+            # End game if score has not been updated after x moves
+            if move_count >= 440:
+                break
+
+            if current_score < game.score:
+                current_score = game.score
+                move_count = 0
+
             state = game.get_state().T
-
             move = np.argmax(network.activate(state))
-            if random.random() < EPSILON:
-                move = random.randint(0, 2)
 
+            # move right
             if move == 1:
                 game.do_move([0, 1, 0])
+            # move left
             elif move == 2:
                 game.do_move([0, 0, 1])
+            # move straight
             else:
                 game.do_move([0, 0, 0])
 
             if display_on:
                 game.update_display()
-                pygame.time.wait(100)
+                pygame.time.wait(10)
+
+            move_count += 1
 
         total_score += game.score
 
@@ -44,7 +53,7 @@ def play(network, num_games, display_on):
 def eval_genomes(genomes, config):
     for genome_id, genome in genomes:
         network = neat.nn.RecurrentNetwork.create(genome, config)
-        genome.fitness = play(network, 10, False)
+        genome.fitness = play(network, 1, False)
 
 
 def run(config_file):
@@ -60,14 +69,11 @@ def run(config_file):
     pop = neat.Population(config)
 
     pop.add_reporter(neat.StdOutReporter(True))
-    stats = neat.StatisticsReporter()
-    pop.add_reporter(stats)
-    # pop.add_reporter(neat.Checkpointer(5))
 
-    best_genome = pop.run(eval_genomes, 100)
+    best_genome = pop.run(eval_genomes, 50)
 
     network = neat.nn.RecurrentNetwork.create(best_genome, config)
-    play(network, 2, True)
+    play(network, 5, True)
 
     pygame.quit()
 
